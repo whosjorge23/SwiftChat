694eed4fb35246d88fd4e5f5f0ad984427839bee
Giorgio Giannotta <jorge_upa@msn.com>
2023-01-21 11:07:22 +0100

    Initial Commit

---
+++ SwiftChat/SwiftChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChatApp.swift
+//  SwiftChat
+//
+//  Created by Giorgio Giannotta on 21/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
